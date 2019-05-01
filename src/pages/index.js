54d16909b50fec17f22b455e20d5221b1ac9bc4c
KatsuyaGoto <gotokatsuya@gotokatsuya.local>
2019-05-01 18:19:09 +0900

    Fix config

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <SEO title="ME" keywords={[`gotokatsuya`]} />
+        <SEO title="Me" keywords={[`gotokatsuya`]} />
         <section className="hero is-fullheight">
           <div className="hero-body">
             <div className="container has-text-centered">
